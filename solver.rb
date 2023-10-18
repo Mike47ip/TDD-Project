fd0e0c0fbc9812e1b5c8024aa87166fde1389b53
Mike Dark <agyemankojo47@gmail.com>
2023-10-18 13:03:58 +0000

    Change 'n' to 'num' in solver.rb

--- solver.rb
+++ solver.rb
@@ -1,9 +1,9 @@
 class Solver
   def self.factorial(num)
-    raise ArgumentError, 'Input must be a non-negative integer' if n.negative?
+    raise ArgumentError, 'Input must be a non-negative integer' if num.negative?
 
     result = 1
-    (1..n).each { |i| result *= i }
+    (1..num).each { |i| result *= i }
     result
   end
 
@@ -11,15 +11,15 @@ class Solver
     word.reverse
   end
 
-  def self.fizzbuzz(num)
-    if (n % 3).zero? && (n % 5).zero?
+  def self.fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
       'fizzbuzz'
-    elsif (n % 3).zero?
+    elsif (number % 3).zero?
       'fizz'
-    elsif (n % 5).zero?
+    elsif (number % 5).zero?
       'buzz'
     else
-      n.to_s
+      number.to_s
     end
   end
 end
